5f1d3f0d2dcdd325c8a8d613ed9ffff0ce5b7c00
klioxide-lol <xzenerne@gmail.com>
2021-10-29 16:34:42 -0500

    Stereo v1.0.0a
    Basic Release

---
+++ Stereo.js
@@ -0,0 +1,37 @@
+// 101% not Afys Stereo plugin
+
+import { after } from "@cumcord/patcher";
+import { findByPrototypes } from "@cumcord/modules/webpack";
+
+export default () => {
+  let voiceModule = findByPrototypes("setSelfDeaf");
+  let voicePatch;
+
+  return {
+    onLoad() {
+      voicePatch = after("initialize", voiceModule.prototype, function(args, resp){
+        const setTransportOptions = this.conn.setTransportOptions;
+        this.conn.setTransportOptions = function (obj) {
+          if (obj.audioEncoder) {
+            obj.audioEncoder.params = {
+              stereo: "4",
+            }
+            obj.audioEncoder.channels = 4;
+          }
+          if (obj.fec) {
+            obj.fec = false;
+          };
+          if (obj.encodingVoiceBitRate < 960000) {
+              obj.encodingVoiceBitRate = 398000;
+          }
+          this.origin(obj);
+          window.sound = this;
+          return setTransportOptions.call(this, obj);
+        }
+      })
+    },
+    onUnload() {
+      voicePatch();
+    }
+  }
+};
